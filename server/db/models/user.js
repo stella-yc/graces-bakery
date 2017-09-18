1dbf782b06658dfe038f657b4da69d5c2cd3a200
Stella Chung <stll.chung@gmail.com>
2017-09-18 12:52:45 -0400

    First, Last name fields to User model

--- server/db/models/user.js
+++ server/db/models/user.js
@@ -5,6 +5,14 @@ const Sequelize = require('sequelize');
 const db = require('../db');
 
 const User = db.define('user', {
+  firstName: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  lastName: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
   email: {
     type: Sequelize.STRING,
     unique: true,
