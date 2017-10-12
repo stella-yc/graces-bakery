c26482a613241978f6f8b96a16ae1e91099b9fd2
Stella Chung <stll.chung@gmail.com>
2017-10-12 15:55:22 -0400

    Adjusted styling of Product-Icon and Product image components

--- client/components/product-icon.js
+++ client/components/product-icon.js
@@ -3,14 +3,14 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
 const ProductIcon = (props) => {
-  let {id, name, description, price, image} = props.prod;
+  let {id, name, price, image} = props.prod;
   return (
     <div className="product-icon">
-      <div className="img-container">
-        <Link to={`/products/${id}`}>
-          <img src={image} />
-        </Link>
-      </div>
+      <Link to={`/products/${id}`}>
+        <div
+          className="img-container"
+          style={{ backgroundImage: `url(${image})`}} />
+      </Link>
       <h3>{name}</h3>
       <p>{`$${price}`}</p>
     </div>
