34fdf9a7292eace0745fac54bd13cf0d725c9dbe
Stella Chung <stll.chung@gmail.com>
2017-10-13 16:24:44 -0400

    Refactored Products Component

--- client/components/products.js
+++ client/components/products.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+
 import { categoryProducts } from '../store';
-import { withRouter, Link } from 'react-router-dom';
 import ProductIcon from './product-icon';
 
 /*** COMPONENT ***/
@@ -13,55 +14,70 @@ export class Products extends Component {
       showProducts: false
     };
   }
+
   componentDidMount () {
-    this.props.categoryProducts(this.props.match.params.cid);
+    this.props.fetchProducts(this.props.match.params.cid);
     window.scrollTo(0, 0);
   }
 
   componentWillReceiveProps(nextProps) {
-    console.log('match', this.props.match.params.cid);
     if (nextProps.match.params.cid !== this.props.match.params.cid) {
-      this.props.categoryProducts(nextProps.match.params.cid);
+      this.props.fetchProducts(nextProps.match.params.cid);
     }
-    if (nextProps.categories.products) {
+    if (nextProps.products) {
       this.setState({showProducts: true});
     }
   }
-  render() {
-    const { categories } = this.props;
 
-    if (!categories.products) {
+  render() {
+    const { category, products } = this.props;
+    if (!products) {
       return null;
     }
     return (
       <div>
         <div className="cat-description">
-          <h1>{categories.displayName}</h1>
-          <p>{categories.description}</p>
+          <h1>{category.displayName}</h1>
+          <p>{category.description}</p>
         </div>
         <div className="prod-container">
-
         {
-          categories.products.map(prod => {
-            return <ProductIcon key={prod.id} prod={prod} />;
-          })
+          products.map(prod =>
+            <ProductIcon key={prod.id} prod={prod} />
+          )
         }
         </div>
       </div>
     );
   }
-};
+}
 
 /*** CONTAINER ***/
 const mapState = (state) => {
   return {
-    categories: state.productsByCat
+    category: {
+      name: state.productsByCat.name,
+      displayName: state.productsByCat.displayName,
+      description: state.productsByCat.description,
+      id: state.productsByCat.id
+    },
+    products: state.productsByCat.products
   };
 };
 
-export default withRouter(connect(mapState, {categoryProducts})(Products));
+const mapDispatch = (dispatch) => {
+  return {
+    fetchProducts (cid) {
+      dispatch(categoryProducts(cid));
+    }
+  };
+};
+
+export default withRouter(connect(mapState, mapDispatch)(Products));
 
 /*** PROP TYPES ***/
 Products.propTypes = {
+  category: PropTypes.object.isRequired,
   products: PropTypes.array,
+  fetchProducts: PropTypes.func.isRequired,
 };
