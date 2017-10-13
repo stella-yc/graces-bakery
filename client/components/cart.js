4da50b0f606f9ac4cb95abe7646de523c00b2f35
Stella Chung <stll.chung@gmail.com>
2017-10-13 15:12:09 -0400

    Separated CartTable into separate file. mapDispatch, Proptypes, and code formatting

--- client/components/cart.js
+++ client/components/cart.js
@@ -1,91 +1,52 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { callGetCart } from '../store';
 import { withRouter, Link } from 'react-router-dom';
 
-import CartIcon from './cart-icon';
+import { callGetCart } from '../store';
+import CartTable from './cart-table';
 
-const CartTable = (props) => {
-  const { products } = props;
-  if (!products || products.length === 0) {
-    return (
-      <div>
-        <h2>Your Cart is Empty!</h2>
-        <h2>:(</h2>
-      </div>
-    );
-  }
-  return (
-    <table className="cart-table">
-      <thead className="cart-table-header">
-      <tr>
-        <th></th>
-        <th>Name</th>
-        <th>Price</th>
-        <th>Quantity</th>
-      </tr>
-    </thead>
-    <tbody>
-    {
-      products && products.map(prod => (
-        <CartIcon
-          key={prod.id}
-          product={prod}
-        />
-      ))
-    }
-    </tbody>
-    </table>
-  );
-};
 
 /*** COMPONENT ***/
-
 export class Cart extends Component {
   constructor(props) {
     super(props);
     this.state = {
       showCart: false,
-      subTotal: 0
+      subTotal: 0.00
     };
     this.calculateSubTotal = this.calculateSubTotal.bind(this);
   }
 
   componentDidMount () {
-    if (this.props.user.id) {
-      this.props.callGetCart();
-    }
-    if (this.props.cart.id) {
-      this.calculateSubTotal(this.props.cart);
+    const { fetchCartInfo, cart } = this.props;
+    fetchCartInfo();
+    if (cart.id) {
+      this.calculateSubTotal(cart);
     }
     window.scrollTo(0, 0);
   }
 
   componentWillReceiveProps (nextProps) {
     if (nextProps.user.id !== this.props.user.id) {
-      this.props.callGetCart(nextProps.user.id);
+      this.props.fetchCartInfo(nextProps.user.id);
     }
-    if (nextProps.cart.id) {
-      this.calculateSubTotal(nextProps.cart);
-    }
-    if (nextProps.cart.id !== this.props.cart.id) {
+    if (nextProps.cart.id || nextProps.cart.id !== this.props.cart.id) {
       this.calculateSubTotal(nextProps.cart);
       this.setState({showCart: true});
     }
   }
 
   calculateSubTotal (cart) {
-    console.log('calculateSubTotal called');
     if (!cart.products) {
-      return this.setState({ subTotal: 0 });
+      return this.setState({ subTotal: 0.00 });
     }
     let subTotal = cart.products.reduce((acc, prod) => {
       let sum = prod.price * prod.CartDetail.quantity;
       return acc + sum;
     }, 0);
     subTotal = parseFloat(Math.round(subTotal * 100) / 100).toFixed(2);
-    this.setState({ subTotal: subTotal });
+    this.setState({ subTotal });
   }
 
   render() {
@@ -98,14 +59,12 @@ export class Cart extends Component {
           <CartTable
             products={cart.products}
           />
-          <div>
-            <h4 className="cart-subtotal">
-              <b>SubTotal:</b> ${this.state.subTotal}
-            </h4>
-            <Link to="/checkout">
-              <button className="checkout-btn">Checkout</button>
-            </Link>
-          </div>
+          <h4 className="cart-subtotal">
+            <b>SubTotal:</b> ${this.state.subTotal}
+          </h4>
+          <Link to="/checkout">
+            <button className="checkout-btn">Checkout</button>
+          </Link>
         </div>
       </div>
     );
@@ -115,15 +74,23 @@ export class Cart extends Component {
 /*** CONTAINER ***/
 const mapState = (state) => {
   return {
-    categories: state.productsByCat,
     user: state.user,
     cart: state.cart
   };
 };
 
-export default withRouter(connect(mapState, { callGetCart })(Cart));
+const mapDispatch = dispatch => {
+  return {
+    fetchCartInfo: () =>
+      dispatch(callGetCart())
+  };
+};
+
+export default withRouter(connect(mapState, mapDispatch)(Cart));
 
 /*** PROP TYPES ***/
 Cart.propTypes = {
-
+  user: PropTypes.object.isRequired,
+  cart: PropTypes.object.isRequired,
+  fetchCartInfo: PropTypes.func.isRequired
 };
