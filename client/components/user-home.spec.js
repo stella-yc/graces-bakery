22360b354edd461b43cc1ec9954a2bf13d452f14
Stella Chung <stll.chung@gmail.com>
2017-10-15 18:45:09 -0400

    Unit testing for UserHome component

--- client/components/user-home.spec.js
+++ client/components/user-home.spec.js
@@ -3,16 +3,36 @@
 import { expect } from 'chai';
 import React from 'react';
 import { shallow } from 'enzyme';
+import sinon from 'sinon';
+
 import { UserHome } from './user-home';
 
 describe('UserHome', () => {
   let userHome;
+  const dummyUser = {
+    firstName: 'Spongebob',
+    lastName: 'Squarepants',
+    email: 'bikini@bottom.com'
+  };
+  let logoutSpy = sinon.spy();
 
   beforeEach(() => {
-    userHome = shallow(<UserHome email={'cody@email.com'} />)
+    userHome = shallow(
+      <UserHome
+        user={dummyUser}
+        handleLogout={logoutSpy}
+      />
+    );
   });
 
-  it('renders the email in an h3', () => {
-    expect(userHome.find('h3').text()).to.be.equal('Welcome, cody@email.com');
+  it('renders the user information', () => {
+    expect(userHome.text()).to.have.string('Spongebob');
+    expect(userHome.text()).to.have.string('Squarepants');
+    expect(userHome.text()).to.have.string('bikini@bottom.com');
+  });
+
+  it('clicking logout will invoke handleLogout', () => {
+    userHome.find('.logout-btn').simulate('click');
+    expect(logoutSpy.called).to.be.equal(true);
   });
 });
